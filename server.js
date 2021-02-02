ed752d453bcfb17bdbaca9e7b30501635f29fcb1
abrave1113 <abrave1113@gmail.com>
2021-02-02 12:34:31 -0500

    track error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
  
-const mongoose = require('mongoose');
+let mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
