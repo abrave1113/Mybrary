d9fe70cb055203510a2926725f47f67f80435a44
abrave1113 <abrave1113@gmail.com>
2021-02-04 22:13:46 -0500

    books routes

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const expressLayouts = require('express-ejs-layouts');
-const Author = require('../models/book');
+const Book = require('../models/book');
+const Author = require('../models/author');
 let mongoose = require('mongoose');
 
 //All Books Route
@@ -10,8 +11,18 @@ router.get('/', async (req, res) => {
 });
 
 //New Book Route
-router.get('/new', (req, res) => {
-	res.send('New Book')
+router.get('/new', async (req, res) => {
+	try {
+		const authors = await Author.find({})
+		const book = new Book ()
+		res.render("books/new", { 
+			authors: authors, 
+			book: book
+		})
+	}
+	catch {
+		res.redirect("books")
+	}
 });
 
 //Create Book Route
