6f09e2b20b636dde40381ec7b145edb0bcdf7b9d
abrave1113 <abrave1113@gmail.com>
2021-02-08 14:24:05 -0500

    track deploy error

--- routes/books.js
+++ routes/books.js
@@ -19,7 +19,7 @@ const upload = multer({
 
 //All Books Route
 router.get('/', async (req, res) => {
-	let query = await Book.find()
+	let query = Book.find()
 	if (req.query.title != null && req.query.title != '') {
 		query = query.regex('title', new RegExp(req.query.title, 'i'))
 	}
