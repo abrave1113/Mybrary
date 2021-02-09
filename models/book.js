2ad3bbae1ea70d660a27b2a0979b7bdf5ecbdb75
abrave1113 <abrave1113@gmail.com>
2021-02-09 15:50:54 -0500

    clean up covers

--- models/book.js
+++ models/book.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const path = require('path')
-const coverImageBasePath = 'uploads/bookCovers'
 
 const bookSchema = new mongoose.Schema({
 	title: {
@@ -45,6 +43,6 @@ bookSchema.virtual('coverImagePath').get(function () {
 });
 
 module.exports = mongoose.model('Book', bookSchema);	
-module.exports.coverImageBasePath = coverImageBasePath;
+// module.exports.coverImageBasePath = coverImageBasePath;
 
 // save version
--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const fs = require ('fs');
 const expressLayouts = require('express-ejs-layouts');
 const Book = require('../models/book');
-const uploadPath = path.join('public', Book.coverImageBasePath)
 const Author = require('../models/author');
 const mongoose = require('mongoose');
 const imageMimeTypes = ['image/jpeg','image/png','image/gif' ]
